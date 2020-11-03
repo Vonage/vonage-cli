94fda73d2a88b7d650349107c965565d6dd0b888
Luke Oliff <luke@lukeoliff.com>
2020-11-03 10:15:14 +0000

    quick structure for libs

--- packages/numbers/src/commands/number/list.ts
+++ packages/numbers/src/commands/number/list.ts
@@ -1,35 +1,37 @@
 import {Command, flags} from '@oclif/command'
+import Numbers from '../../libs/numbers'
 
 export default class List extends Command {
-  static description = 'list vonage numbers'
+  static description = 'list vonage number'
 
   static examples = [
     `$ vonage number:list
-list of numbers?
+list all numbers
 `,
   ]
 
   static flags = {
     help: flags.help({char: 'h'}),
     app: flags.boolean({char: 'n', description: 'list app numbers'}),
+    sip: flags.boolean({char: 't', description: 'list sip numbers'}),
     sms: flags.boolean({char: 's', description: 'list sms numbers'}),
     tel: flags.boolean({char: 't', description: 'list tel numbers'}),
-    sip: flags.boolean({char: 't', description: 'list sip numbers'}),
   }
 
   async run() {
     const {flags} = this.parse(List)
+    const numbers = new Numbers()
 
     if (flags.app) {
-      this.log('app list')
-    } else if (flags.sms) {
-      this.log('sms list')
-    } else if (flags.tel) {
-      this.log('tel list')
+      this.log(numbers.list('app'))
     } else if (flags.sip) {
-      this.log('sip list')
+      this.log(numbers.list('sip'))
+    } else if (flags.sms) {
+      this.log(numbers.list('sms'))
+    } else if (flags.tel) {
+      this.log(numbers.list('tel'))
     } else {
-      this.log('list all numbers')
+      this.log(numbers.list('all'))
     }
   }
 }
