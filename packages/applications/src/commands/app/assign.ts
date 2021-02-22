e750f179c01f0c50728be23b66fc0a02dc313d93
Kelly J Andrews <kelly@kellyjandrews.com>
2021-02-22 11:57:30 -0500

    update ts lib

--- packages/applications/src/commands/app/assign.ts
+++ packages/applications/src/commands/app/assign.ts
@@ -11,19 +11,31 @@ export default class ApplicationsAssign extends Command {
 
     static args = [
         { name: 'appId', required: false },
-        { name: 'lvn', required: false }
+        { name: 'number', required: false }
     ]
 
     async run() {
         const { args, flags }: { args: any, flags: { [index: string]: any } } = this.parse(ApplicationsAssign)
 
+        // if no args provided, present UX
+        if (!args.appId && !args.lvn) {
+            //get list of applications - select one
+            //get list of numbers - select multiple?
+        }
+
+        // check for either arg, if any missing, error out
+        if (!args.appId || !args.lvn) {
+            this.log('Please provide both Application ID and Number')
+            this.exit();
+        }
+
         // get the number details, or error if number doesn't exist
-        let number = await this.listNumbers(args.lvn)
+        let number = await this.listNumbers(args.number)
 
         // update the number with appid, lvn, country
-        let response = await this.updateNumber(args.lvn, number.numbers[0].country, args.appId)
+        let response = await this.updateNumber(args.number, number.numbers[0].country, args.appId)
         if (response['error-code'] === '200') {
-            this.log(`Number '${args.lvn}' is assigned to '${args.appId}'.`);
+            this.log(`Number '${args.number}' is assigned to '${args.appId}'.`);
             this.exit()
         }
         this.log(response);
