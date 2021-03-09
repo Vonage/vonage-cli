9ea04b7a648513caacbe7d372097ab2779f48d43
Chris Tankersley <chris@ctankersley.com>
2021-03-09 13:26:20 -0500

    Added new balance command

---
+++ packages/utils/src/balance_base.ts
@@ -0,0 +1,23 @@
+import BaseCommand from './command';
+
+export default abstract class BalanceCommand extends BaseCommand {
+    static flags = {
+        ...BaseCommand.flags,
+    };
+
+    static args = [
+        ...BaseCommand.args,
+    ];
+
+    displayBalance(): any {
+        return new Promise((res, rej) => {
+            this.vonage.account.checkBalance((error: any, response: any) => {
+                if(error) {
+                    rej(error);
+                } else {
+                    res(response);
+                }
+            });
+        })
+    }
+}
