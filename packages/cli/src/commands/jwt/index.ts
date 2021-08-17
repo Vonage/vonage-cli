f1f796ca815568aeda1064e2efb9812a3e092cc9
Kelly J Andrews <kelly@kellyjandrews.com>
2021-08-17 10:36:38 -0400

    fix jwt method

--- packages/cli/src/commands/jwt/index.ts
+++ packages/cli/src/commands/jwt/index.ts
@@ -34,7 +34,7 @@ export default class GenerateJWT extends BaseCommand {
     async run() {
 
         const flags = this.parsedFlags as OutputFlags<typeof BaseCommand.flags> & GenerateJWTFlags
-        let private_key = readFileSync(flags.key_file.replace(/(\s+)/g, '\\$1')).toString();
+        let private_key = readFileSync(flags.key_file.replace(/(\s+)/g, '\\$1'));
         let claims = { application_id: flags.app_id, sub: flags.subject, acl: JSON.parse(flags.acl) }
         let jwt = this.Vonage.generateJwt(private_key, claims)
 
