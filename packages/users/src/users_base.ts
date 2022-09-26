7337448b664b98c1a6bbb2454324647606dab718
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2022-09-26 15:52:59 +0000

    style: users linter errors

--- packages/users/src/users_base.ts
+++ packages/users/src/users_base.ts
@@ -7,11 +7,13 @@ import { readFileSync } from 'fs';
 import { merge } from 'lodash';
 import { HTTPMethods, ResponseTypes } from './types';
 
-export default abstract class UsersCommand<T extends typeof BaseCommand.flags> extends BaseCommand<T> {
-    protected _token!: string
+export default abstract class UsersCommand<
+    T extends typeof BaseCommand.flags,
+> extends BaseCommand<T> {
+    protected _token!: string;
     protected _baseurl = `https://api.nexmo.com/v0.3/users`;
-    protected parsedArgs
-    protected parsedFlags
+    protected parsedArgs;
+    protected parsedFlags;
 
     static flags: OutputFlags<typeof BaseCommand.flags> = {
         ...BaseCommand.flags,
@@ -22,15 +24,18 @@ export default abstract class UsersCommand<T extends typeof BaseCommand.flags> e
     };
 
     protected _defaultHttpOptions = {
-        "method": HTTPMethods.GET,
-        "headers": {},
-        'responseType': ResponseTypes.json
-    }
+        method: HTTPMethods.GET,
+        headers: {},
+        responseType: ResponseTypes.json,
+    };
 
     private async _generateJWT() {
-        let app_details_raw = readFileSync(`${process.cwd()}/vonage_app.json`);
-        let app_details = (JSON.parse(app_details_raw.toString()));
-        this._token = await tokenGenerate(app_details.application_id, app_details.private_key)
+        const appDetailsRaw = readFileSync(`${process.cwd()}/vonage_app.json`);
+        const appDetails = JSON.parse(appDetailsRaw.toString());
+        this._token = await tokenGenerate(
+            appDetails.application_id,
+            appDetails.private_key,
+        );
         return;
     }
 
@@ -45,47 +50,47 @@ export default abstract class UsersCommand<T extends typeof BaseCommand.flags> e
     }
 
     async getAllUsers(params) {
-        const opts = merge({}, this._defaultHttpOptions)
+        const opts = merge({}, this._defaultHttpOptions);
         opts['url'] = `${this._baseurl}`;
-        opts['headers']['Authorization'] = `Bearer ${this._token}`
-        let response = await request(opts);
+        opts['headers']['Authorization'] = `Bearer ${this._token}`;
+        const response = await request(opts);
         return response;
     }
 
     async getUserById(id) {
-        const opts = merge({}, this._defaultHttpOptions)
+        const opts = merge({}, this._defaultHttpOptions);
         opts['url'] = `${this._baseurl}/${id}`;
-        opts['headers']['Authorization'] = `Bearer ${this._token}`
-        let response = await request(opts);
-        return response
+        opts['headers']['Authorization'] = `Bearer ${this._token}`;
+        const response = await request(opts);
+        return response;
     }
 
     async createUser(params) {
-        const opts = merge({}, this._defaultHttpOptions)
+        const opts = merge({}, this._defaultHttpOptions);
         opts['url'] = `${this._baseurl}`;
         opts['method'] = HTTPMethods.POST;
         opts['data'] = params;
-        opts['headers']['Authorization'] = `Bearer ${this._token}`
-        let response = await request(opts);
-        return response
+        opts['headers']['Authorization'] = `Bearer ${this._token}`;
+        const response = await request(opts);
+        return response;
     }
 
     async updateUser(params) {
-        const opts = merge({}, this._defaultHttpOptions)
+        const opts = merge({}, this._defaultHttpOptions);
         opts['url'] = `${this._baseurl}/${params.userID}`;
         opts['method'] = HTTPMethods.PATCH;
         opts['data'] = params;
-        opts['headers']['Authorization'] = `Bearer ${this._token}`
-        let response = await request(opts);
-        return response
+        opts['headers']['Authorization'] = `Bearer ${this._token}`;
+        const response = await request(opts);
+        return response;
     }
 
     async deleteUser(id) {
-        const opts = merge({}, this._defaultHttpOptions)
+        const opts = merge({}, this._defaultHttpOptions);
         opts['url'] = `${this._baseurl}/${id}`;
         opts['method'] = HTTPMethods.DELETE;
-        opts['headers']['Authorization'] = `Bearer ${this._token}`
-        let response = await request(opts);
-        return response
+        opts['headers']['Authorization'] = `Bearer ${this._token}`;
+        const response = await request(opts);
+        return response;
     }
 }
