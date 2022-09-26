13c85ba40b3c9f7e4778856b45a645a6ba26a925
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2022-09-26 17:34:11 +0000

    build: tool cleanup

--- packages/cli/src/hooks/init.ts
+++ packages/cli/src/hooks/init.ts
@@ -1,41 +1,46 @@
-import { Hook } from '@oclif/config'
-import { promises as fs } from 'fs'
-import { writeFileSync, constants } from 'fs'
-import * as path from 'path'
+import { Hook } from '@oclif/config';
+import { promises as fs } from 'fs';
+import { writeFileSync, constants } from 'fs';
+import * as path from 'path';
 import shell from 'shelljs';
 
-async function isExists(path: any) {
+async function isExists(path: string) {
     try {
-        let result = await fs.access(path, constants.F_OK);
+        // FIXME use builtin
+        await fs.access(path, constants.F_OK);
         return true;
     } catch (error) {
         return false;
     }
-};
+}
 
-async function writeFile(filePath: any, data: any) {
+async function writeFile(filePath: string) {
     try {
         const dirname = path.dirname(filePath);
         let exist = await isExists(dirname);
 
         if (!exist) {
+            // FIXME use path instead of shell
             await shell.mkdir('-p', dirname);
         }
 
         exist = await isExists(filePath);
         if (!exist) {
-            writeFileSync(filePath, JSON.stringify({ apiKey: '', apiSecret: '' }, null, 2), 'utf8');
+            writeFileSync(
+                filePath,
+                JSON.stringify({ apiKey: '', apiSecret: '' }, null, 2),
+                'utf8',
+            );
         }
-
     } catch (err) {
         throw new Error(err);
     }
 }
 
-const hook: Hook<'init'> = async function (options) {
-    let name = path.join(this.config.configDir, 'vonage.config.json');
-    writeFile(name, '');
+const hook: Hook<'init'> = async function(options) {
+    const name = path.join(this.config.configDir, 'vonage.config.json');
+    writeFile(name);
     return;
-}
+};
 
 export default hook;
