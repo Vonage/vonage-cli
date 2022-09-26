6d8dac9c1fd51bb38afcbba4b86d79dbce9617e9
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2022-09-26 16:08:31 +0000

    style: cli linter errors

--- packages/cli/src/commands/jwt/index.ts
+++ packages/cli/src/commands/jwt/index.ts
@@ -3,34 +3,30 @@ import { flags } from '@oclif/command';
 import { OutputFlags } from '@oclif/parser';
 import { readFileSync } from 'fs';
 
-
-
 interface GenerateJWTFlags {
-    app_id: any,
-    key_file: any,
-    subject?: any,
-    acl?: any,
-    exp?: any
+    app_id: any;
+    key_file: any;
+    subject?: any;
+    acl?: any;
+    exp?: any;
 }
 
-
 export default class GenerateJWT extends BaseCommand<typeof GenerateJWT.flags> {
-
-    static description = 'generate a Vonage JWT token'
+    static description = 'generate a Vonage JWT token';
 
     static examples = [
-        `jwt --key_file='./testing.key' --app_id=31521081-e4c7-41fe-bccc-44af98879068`
-    ]
+        `jwt --key_file='./testing.key' --app_id=31521081-e4c7-41fe-bccc-44af98879068`,
+    ];
 
     static flags: OutputFlags<typeof BaseCommand.flags> & GenerateJWTFlags = {
         ...BaseCommand.flags,
         app_id: flags.string({
             required: true,
-            description: "Application ID to authenticate with"
+            description: 'Application ID to authenticate with',
         }),
         key_file: flags.string({
             required: true,
-            description: "Path to private key file location"
+            description: 'Path to private key file location',
         }),
         subject: flags.string({
             required: false,
@@ -39,27 +35,31 @@ export default class GenerateJWT extends BaseCommand<typeof GenerateJWT.flags> {
             required: false,
             description: `Read more about it in the ACL overview - https://developer.vonage.com/conversation/guides/jwt-acl#acls`,
         }),
-        exp: flags.string({ required: false, description: "Expiration of created JWT - defaults to 24 hours." }),
-    }
+        exp: flags.string({
+            required: false,
+            description: 'Expiration of created JWT - defaults to 24 hours.',
+        }),
+    };
 
-    static args = [
-    ]
+    static args = [];
 
     async run() {
         const flags = this.parsedFlags;
-        let private_key = typeof flags.key_file === 'string' ? readFileSync(flags.key_file.replace(/(\s+)/g, '\\$1')) : null;
-        let claims = {
+        // prettier-ignore
+        const privateKey = typeof flags.key_file === 'string'
+            ? readFileSync(flags.key_file.replace(/(\s+)/g, '\\$1'))
+            : null;
+        const claims = {
             application_id: flags.app_id,
             sub: flags.subject,
-            exp: flags.exp || Date.now() + 21600
-        }
+            exp: flags.exp || Date.now() + 21600,
+        };
 
-        flags.acl ? claims['acl'] = JSON.parse(flags.acl.toString()) : null;
+        flags.acl ? (claims['acl'] = JSON.parse(flags.acl.toString())) : null;
 
-        let jwt = this.Vonage.generateJwt(private_key, claims)
+        const jwt = this.Vonage.generateJwt(privateKey, claims);
 
-        this.log(jwt)
-        this.exit()
-
+        this.log(jwt);
+        this.exit();
     }
 }
