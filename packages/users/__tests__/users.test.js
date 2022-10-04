0855158231eddfcfa6e744fee97ae2abdf00d522
Chuck Reeves <manchuck@users.noreply.github.com>
2022-10-04 10:20:38 -0400

    style: users linter errors (#84)

--- packages/users/__tests__/users.test.js
+++ packages/users/__tests__/users.test.js
@@ -1,3 +1,46 @@
-describe('users CLI Commands', () => {
-    test.todo('test something on users')
-})
+import tslibOne from 'tslib';
+import testOne from '@oclif/test';
+import fsOne from 'fs';
+
+exports.appFileData = {
+    application_name: 'Test App File',
+    application_id: '1234-5678-abcd-efgh',
+    private_key:
+        '-----BEGIN PRIVATE KEY-----not a real key----END PRIVATE KEY-----\n',
+};
+
+// eslint-disable-next-line no-undef
+describe('users vonage_app.json file checks', () => {
+    // eslint-disable-next-line no-undef
+    beforeEach(() =>
+        tslibOne.__awaiter(void 0, void 0, void 0, function* () {
+            try {
+                yield fsOne.promises.unlink('vonage_app.json');
+            } catch (error) {
+                console.error(error);
+            }
+        }),
+    );
+    // eslint-disable-next-line no-undef
+    afterEach(() =>
+        tslibOne.__awaiter(void 0, void 0, void 0, function* () {
+            try {
+                yield fsOne.promises.writeFile(
+                    'vonage_app.json',
+                    JSON.stringify(exports.appFileData, null, 2),
+                );
+            } catch (error) {
+                console.error(error);
+            }
+        }),
+    );
+    testOne.test
+        .env({ VONAGE_API_KEY: '12345', VONAGE_API_SECRET: 'ABCDE' })
+        .stdout()
+        .command(['apps:users'])
+        .it('should gracefully fail if no app file', (ctx) => {
+            (0, testOne.expect)(ctx.stdout).to.equal(
+                `Please create an application or a vonage_app.json file to proceed.\n`,
+            );
+        });
+});
--- packages/users/src/commands/apps/users/create.ts
+++ packages/users/src/commands/apps/users/create.ts
@@ -1,5 +1,5 @@
 import { OutputFlags } from '@oclif/parser';
-import { flags } from '@oclif/command'
+import { flags } from '@oclif/command';
 import { ArgInput } from '@oclif/core/lib/interfaces';
 import UserCommand from '../../../users_base';
 import cli from 'cli-ux';
@@ -7,44 +7,52 @@ import chalk from 'chalk';
 import { VetchResponse } from '../../../types';
 
 interface CreateFlags {
-    display_name: any
-    image_url: any
+    display_name: any;
+    image_url: any;
 }
 
 export default class UsersCreate extends UserCommand<typeof UsersCreate.flags> {
-    static description = ""
+    static description = '';
 
-    static examples = [
-    ]
+    static examples = [];
 
     static flags: OutputFlags<typeof UserCommand.flags> & CreateFlags = {
         ...UserCommand.flags,
-        'display_name': flags.string({ description: '' }),
-        'image_url': flags.string({ description: '' }),
-    }
+        display_name: flags.string({ description: '' }),
+        image_url: flags.string({ description: '' }),
+    };
 
-    static args: ArgInput = [
-        { name: 'name', required: false }
-    ]
+    static args: ArgInput = [{ name: 'name', required: false }];
 
     async run() {
-        const flags = this.parsedFlags
+        const flags = this.parsedFlags;
         const args = this.parsedArgs!;
 
         // check for name
 
-        cli.action.start(chalk.bold('Creating User'), 'Initializing', { stdout: true })
+        cli.action.start(chalk.bold('Creating User'), 'Initializing', {
+            stdout: true,
+        });
 
-        let response = await this.createUser({ ...flags, ...args }) as VetchResponse;
+        const response = (await this.createUser({
+            ...flags,
+            ...args,
+        })) as VetchResponse;
 
-        cli.action.stop()
+        cli.action.stop();
 
-        this.log(chalk.magenta.underline.bold("User ID:"), response.data.id)
-        this.log('')
-        this.log(chalk.magenta.underline.bold("Name:"), response.data.name, `(${response.data.display_name || ""})`)
-        this.log('')
-        this.log(chalk.magenta.underline.bold("Image Url:"), response.data.image_url || "None")
-        this.log('')
-
+        this.log(chalk.magenta.underline.bold('User ID:'), response.data.id);
+        this.log('');
+        this.log(
+            chalk.magenta.underline.bold('Name:'),
+            response.data.name,
+            `(${response.data.display_name || ''})`,
+        );
+        this.log('');
+        this.log(
+            chalk.magenta.underline.bold('Image Url:'),
+            response.data.image_url || 'None',
+        );
+        this.log('');
     }
 }
