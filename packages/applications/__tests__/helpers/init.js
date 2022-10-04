5b47c8a697da1e80c04fc8bda58e497f37e38c98
Chuck Reeves <manchuck@users.noreply.github.com>
2022-10-04 10:33:58 -0400

    style: application plugin linter errors (#79)

--- packages/applications/__tests__/helpers/init.js
+++ packages/applications/__tests__/helpers/init.js
@@ -1,4 +1,5 @@
-const path = require('path')
+import path from 'path';
+
 process.env.TS_NODE_PROJECT = path.resolve(
-  '../applications/__tests__/tsconfig.json',
-)
+    '../applications/__tests__/tsconfig.json',
+);
