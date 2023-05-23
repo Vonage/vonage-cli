0db75133579e288cba544579ad432318c86b16e9
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2023-05-23 19:24:34 +0000

    test: added tests for file system

---
+++ packages/cli-core/lib/fsUtils.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file: Mocking the filesystem is not fun  */
+import { mkdirSync, writeFileSync, existsSync, readFileSync } from 'fs';
+
+export const readFile = (file: string): string => readFileSync(file).toString();
+
+export const writeFile = (file: string, dataToWrite: string) =>
+  writeFileSync(file, dataToWrite);
+
+export const dirExists = (dir: string): boolean => existsSync(dir);
+
+export const createDir = (dir: string) => mkdirSync(dir, { recursive: true });
