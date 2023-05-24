bcc6ce065dafde8d5118dcd28e35fbb80cd62515
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2023-05-24 19:57:33 +0000

    build: fixing dev cli script

--- packages/cli/bin/dev.js
+++ packages/cli/bin/dev.js
@@ -1,17 +1,30 @@
-#!/usr/bin/env node
+#!/usr/bin/env ts-node
 
-const oclif = require('@oclif/core')
+/* eslint-disable node/shebang */
 
-const path = require('path')
-const project = path.join(__dirname, '..', 'tsconfig.json')
+import oclif from '@oclif/core'
+import path from 'node:path'
+import url from 'node:url'
+// eslint-disable-next-line node/no-unpublished-import
+import { register } from 'ts-node'
 
 // In dev mode -> use ts-node and dev plugins
-process.env.NODE_ENV = 'development'
 
-require('ts-node').register({ project })
+register({
+  project: path.join(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    '..',
+    '..',
+    '..',
+    'tsconfig.json'
+  ),
+})
 
 // In dev mode, always show stack traces
 oclif.settings.debug = true
 
 // Start the CLI
-oclif.run().then(oclif.flush).catch(oclif.Errors.handle)
+oclif
+  .run(process.argv.slice(2), import.meta.url)
+  .then(oclif.flush)
+  .catch(oclif.Errors.handle)
