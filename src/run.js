e654c9196380af38eb789e96cc395639d9413965
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2024-08-07 11:22:16 -0400

    refactor: pulled out vonage config middleware

--- src/run.js
+++ src/run.js
@@ -1,48 +1,7 @@
 #!/usr/bin/env -S node
 const { hideBin } = require('yargs/helpers');
 const yargs = require('yargs');
-const { Auth } = require('@vonage/auth');
-const rc = require('rc');
-
-const getVonageAuth = async (argv) => {
-  // Use any of the args passed in
-  if (argv.apiKey || argv.apiSecret || argv.privateKey || argv.appId) {
-    return {
-      Auth: new Auth({
-        apiKey: argv['api-key'],
-        apiSecret: argv['api-secret'],
-        privateKey: argv['private-key'],
-        applicationId: argv['app-id'],
-      })
-    };
-  }
-
-  // TODO: Find global config
-  // Check XDG_CONFIG_HOME and the windows one (rc will not this)
-
-  // TODO Find nexmo cli config
-  const authConfig = rc('vonage',{});
-  if (!authConfig) {
-    return {};
-
-  }
-  const normalConfig = Object.fromEntries(
-    Object.entries(authConfig).map(
-    ([key, value]) => [
-      key.toUpperCase().replace(/-/g, '_'),
-      value
-    ])
-  );
-
-  return {
-    Auth: new Auth({
-      apiKey: normalConfig.API_KEY,
-      apiSecret: normalConfig.API_SECRET,
-      privateKey: normalConfig.PRIVATE_KEY,
-      applicationId: normalConfig.APP_ID,
-    })
-  };
-};
+const { getVonageAuth } = require('./middleware/vonageAuth');
 
 yargs(hideBin(process.argv))
   .options({
