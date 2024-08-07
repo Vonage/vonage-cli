b3ff4a375fc815162df204250acf606862f5ab08
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2024-08-07 15:37:19 -0400

    build: updated rules for linter

--- eslint.config.js
+++ eslint.config.js
@@ -17,15 +17,20 @@ module.exports = [
       globals: {
         ...globals.node,
         ...globals.jest,
-      }
+      },
     },
     plugins: {
-     '@stylistic/js': stylisticJs
+      '@stylistic/js': stylisticJs,
     },
     rules: {
       '@stylistic/js/semi': ['error', 'always'],
-
-    }
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/js/function-call-argument-newline': ['error', 'consistent'],
+      '@stylistic/js/array-bracket-newline': ['error', { 'multiline': true }],
+      '@stylistic/js/dot-location': ['error', 'property'],
+    },
   },
   {
     files: ['packages/*/src/**/*.{js}'],
@@ -34,15 +39,16 @@ module.exports = [
     settings: {
       node: {
         version: '>=18.0.0',
-      }
+      },
     },
     rules: {
       // Leave this off. This rule cannot handle monorepos
       'n/no-missing-import': ['off'],
       'n/no-unsupported-features/es-builtins': [
         'error', {
-          'ignores': []
-        }]
+          'ignores': [],
+        },
+      ],
     },
   },
 
--- src/run.js
+++ src/run.js
@@ -1,39 +1,54 @@
 #!/usr/bin/env -S node
-const { hideBin } = require('yargs/helpers');
-const yargs = require('yargs');
-const { getVonageAuth } = require('./middleware/vonageAuth');
-const { setupLog } = require('./middleware/log');
+const { hideBin, } = require('yargs/helpers',);
+const yargs = require('yargs',);
+const { getVonageAuth, } = require('./middleware/vonageAuth',);
+const { setupLog, } = require('./middleware/log',);
+const { readFileSync, existsSync, } = require('node:fs',);
 
-yargs(hideBin(process.argv))
+yargs(hideBin(process.argv,),)
   .options({
     'api-key': {
       describe: 'Your Vonage API key',
       type: 'string',
+      group: 'Vonage Credentials:',
+      implies: 'api-secret',
     },
     'api-secret': {
       describe: 'Your Vonage API secret',
       type: 'string',
+      implies: 'api-key',
+      group: 'Vonage Credentials:',
     },
     'private-key': {
       describe: 'Your Vonage private key',
       type: 'string',
+      group: 'Vonage Credentials:',
+      implies: 'app-id',
+      coerce: (arg,) => existsSync(arg,)
+        ? readFileSync(arg, 'utf-8',)
+        : arg,
     },
     'app-id': {
       describe: 'Your Vonage application ID',
+      group: 'Vonage Credentials:',
       type: 'string',
     },
     'verbose': {
+      alias: 'v',
       describe: 'Print more information',
       type: 'boolean',
+      group: 'UX',
     },
     'debug': {
+      alias: 'd',
       describe: 'Print debug information',
       type: 'boolean',
+      group: 'UX',
     },
-  })
-  .middleware(setupLog)
-  .middleware(getVonageAuth)
-  .commandDir('commands')
+  },)
+  .middleware(setupLog,)
+  .middleware(getVonageAuth,)
+  .commandDir('commands', {recurse: true,},)
   .demandCommand()
   .help()
   .parse();
