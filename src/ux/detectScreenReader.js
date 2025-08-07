c830024bdd42115ad4c5b420750c54f4816ff0d9
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2025-08-07 14:58:11 -0400

    docs: added doc blocks to detect screen reader

--- src/ux/detectScreenReader.js
+++ src/ux/detectScreenReader.js
@@ -1,6 +1,14 @@
 const parser = require('yargs-parser');
 const { replayRate, accessibility, plainOutput } = parser(process.argv);
 
+
+/**
+ * Tries to figure out if there is a screen reader in use
+ *
+ * Check using environment variables or if flags are passed in
+ *
+ * @returns { boolean } - True if a reader is detected
+ */
 const detectScreenReader = () => {
   const envVars = process.env;
   const screenReaderEnvVars = ['SCREENREADER', 'ACCESSIBILITY', 'NVDA', 'JAWS', 'VOICEOVER', 'ORCA'];
@@ -18,6 +26,11 @@ const detectScreenReader = () => {
   return false;
 };
 
+/**
+ * Detects if the user wants plain text or not
+ *
+ * @returns { boolean } - True if the user passed in the plain text flag
+ */
 const detectPlainOutput = () => {
   if (plainOutput) {
     return true;
@@ -26,6 +39,11 @@ const detectPlainOutput = () => {
   return detectScreenReader();
 };
 
+/**
+ * Returns a rate at which to refresh dynamic content
+ *
+ * @returns { number } - The number of seconds to refresh
+ */
 const getReplayRate = () => parseInt(replayRate || process.env['REPLAY_RATE'] || 6) * 1000;
 
 module.exports = {
