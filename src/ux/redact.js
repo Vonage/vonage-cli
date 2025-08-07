123b74e36a91f1d7fb33e75854ec496f1f418c99
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2025-08-07 14:59:25 -0400

    docs: added doc blocks to redact

--- src/ux/redact.js
+++ src/ux/redact.js
@@ -1,5 +1,10 @@
-const redact = (text) => text
+/**
+ * Redacts text
+ *
+ * @param { string } text - The text to redact
+ * @returns { string } - The redacted text
+ */
+exports.redact = (text) => text
   ? `${text}`.substring(0, 3) + '*'.repeat(`${text}`.length - 2)
   : null;
 
-exports.redact = redact;
